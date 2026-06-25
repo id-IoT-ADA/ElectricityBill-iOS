71d145232771139aa2812fe8c984da90cbc995ad
ikhwanizh <ikhwaninzaghi999@gmail.com>
2026-06-25 08:35:08 +0800

    init

---
+++ ElectricityBill/ElectricityBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectricityBillApp.swift
+//  ElectricityBill
+//
+//  Created by Ikhwan on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ElectricityBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
