9a379892545482bf809129168864149f12477987
ikhwanizh <ikhwaninzaghi999@gmail.com>
2026-07-02 09:41:49 +0800

    feat: add DeviceListRow component to MonitoringView

--- ElectricityBill/Features/Monitoring/MonitorView.swift
+++ ElectricityBill/Features/Monitoring/MonitorView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct MonitoringView: View {
+    @State var isActive: Bool = false
+    
     var body: some View {
         ZStack {
             Image("OnBoarding Background")
                 .ignoresSafeArea()
             
-            
+            DeviceListRow(isActive: $isActive)
+        }
+    }
+}
+
+struct DeviceListRow : View {
+    @Binding var isActive : Bool
+    
+    var body: some View {
+        HStack {
+            Image("MoneyBag")
+            VStack(alignment: .leading) {
+                Text("Electricity Bill")
+                    .font(.title)
+                    .bold()
+                Text("25 kWh")
+                    .font(.title2)
+            }
+            .padding(.leading, 10)
+            Toggle(isOn: $isActive) {
+                
+            }
         }
     }
 }
